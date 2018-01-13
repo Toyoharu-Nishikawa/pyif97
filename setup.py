588fd658a37ca2ce2b73a0cd9169ba991f1fd72a
toyoharu_nishikawa <toyohal24@hotmail.co.jp>
2018-01-13 23:58:46 +0900

    add dll for package in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(name='pyif97',
       version='1.1',
       packages=['pyif97'],
       package_dir={'pyif97':'pyif97'},
-      package_data={'pyif97':['SteamTable_IF97.so']},
+      package_data={'pyif97':['SteamTable_IF97.so','SteamTable_IF97.dll']},
 )
