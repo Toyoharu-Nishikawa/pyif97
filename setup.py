7365c125a7f5b8eb2e98c8afa4c6610c65291916
toyoharu nishikawa <toyohal@email.com>
2018-02-25 21:22:55 +0900

    update SteamTable.so and dll

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='pyif97',
-      version='1.1',
+      version='1.2',
       packages=['pyif97'],
       package_dir={'pyif97':'pyif97'},
       package_data={'pyif97':['SteamTable_IF97.so','SteamTable_IF97.dll']},
