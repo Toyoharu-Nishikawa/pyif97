bae7c9f2c74651f8f4900a37644a395f25498191
toyoharu nishikawa <toyohal@email.com>
2021-11-20 16:15:17 +0900

    added feature to use numpy

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
+import numpy as np
 from pyif97 import if97
 a=if97.pt2h(1,500)
-mu=if97.pt2mu(10,300)
+b=if97.ps2t(20.1, 6.9)
+p = np.array([1,2,3])
+t = np.array([500,510,520])
+c = if97.pt2h(p,t)
+mu=if97.ph2mu(1,3000,0)
 ver = if97.version()
 print(a)
+print(b)
+print(c)
 print(ver)
 print(mu)
